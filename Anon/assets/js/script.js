7b9af5bb4e13e604c437ab402aaa58b0294b2bf7
princedavid00 <110838505+princedavid00@users.noreply.github.com>
2023-05-19 20:23:21 +0100

    Add files via upload

---
+++ Anon/assets/js/script.js
@@ -0,0 +1,25 @@
+'use strict';
+
+// modal variables
+const modal = document.querySelector('[data-modal]');
+const modaCloseBtn = document.querySelector('[data-modal-close]');
+const modalCloseOverlay = document.querySelector('[data-modal-overlay]');
+
+
+// modal function
+const modalCloseFunc = function () { modal.classList.add('closed') }
+
+// modal eventListener
+modalCloseOverlay.addEventListener('click', modaCloseFunc);
+modalCloseBtn.addEventListener('click', modalCloseFunc);
+
+
+
+// notification toast variables
+const notificationToast = document.querySelector('[data-toast]');
+const toastCloseBtn = document.querySelector('[data-toast-close]');
+
+// notification toast eventListener
+toastCloseBtn.addEventListener('click', function () {
+    notificationToast.classList.add('closed');
+})
